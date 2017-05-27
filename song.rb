f033239819753aa4471b116be226fc7e642dd5ce
goobering <goobering@gmail.com>
2017-05-27 17:08:33 +0100

    Created song_spec.rb

--- song.rb
+++ song.rb
@@ -1,7 +1,11 @@
 class Song
 
-def initialize()
-    
+def initialize(title, artist, album, track_num, lyrics)
+    @title = title
+    @artist = artist
+    @album = album
+    @track_num = track_num
+    @lyrics = lyrics
 end
 
 end
---
+++ specs/song_spec.rb
@@ -0,0 +1,12 @@
+require 'Minitest/autorun'
+require 'Minitest/rg'
+
+require_relative '../song.rb'
+
+class TestSong < Minitest::Test
+
+def setup()
+
+end
+
+end
